5c6054c5c151247e601295bd899f73bbb0237862
Ivan Potapenko <dreamer4ik@gmail.com>
2022-03-08 20:04:50 +0200

    add  app icon

--- Spotify/Resources/AppDelegate.swift
+++ Spotify/Resources/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let window = UIWindow(frame: UIScreen.main.bounds)
         
         if AuthManager.shared.isSignedIn {
+            AuthManager.shared.refreshTokenIFNeeded(completion: nil)
             window.rootViewController = TabBarViewController()
         }
         else {
