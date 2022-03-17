f0d488d463ddf190f97a45b06e9c8b2ea1aa1379
Ivan Potapenko <dreamer4ik@gmail.com>
2022-03-17 19:11:03 +0200

    start putting together the Search User Interface

--- Spotify/Controllers/Other/SearchResultsViewController.swift
+++ Spotify/Controllers/Other/SearchResultsViewController.swift
@@ -11,7 +11,8 @@ class SearchResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.backgroundColor = .systemBackground
       
     }
     
---
+++ Spotify/Views/GenreCollectionViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  GenreCollectionViewCell.swift
+//  Spotify
+//
+//  Created by Ivan Potapenko on 17.03.2022.
+//
+
+import UIKit
+
+class GenreCollectionViewCell: UICollectionViewCell {
+    static let identifier = "GenreCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .white
+        imageView.image = UIImage(systemName: "music.quarternote.3",
+                                  withConfiguration:
+                                    UIImage.SymbolConfiguration(pointSize: 50, weight: .regular))
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        return label
+    }()
+    
+    private let colors: [UIColor] = [
+        .systemPink,
+        .systemBlue,
+        .systemPurple,
+        .systemOrange,
+        .systemGreen,
+        .systemRed,
+        .systemYellow,
+        .darkGray,
+        .systemBrown,
+        .systemTeal
+    ]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        contentView.addSubview(imageView)
+        contentView.addSubview(label)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+//        imageView.image = nil
+        label.text = nil
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = CGRect(x: 10, y: contentView.height/2, width: contentView.width-20, height: contentView.height/2)
+        imageView.frame = CGRect(x: contentView.width/2, y: 0, width: contentView.width/2, height: contentView.height/2)
+    }
+    
+    func configure(with title: String) {
+        label.text = title
+        contentView.backgroundColor = colors.randomElement()
+    }
+}
